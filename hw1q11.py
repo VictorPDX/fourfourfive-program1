c686b03a7417e77310cc2a31dd2e62875a6a6a61
Victor <Victor32@pdx.edu>
2020-02-02 18:49:44 -0800

    cleaned up code to share with group

--- hw1q11.py
+++ hw1q11.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python3
 
 
 import numpy as np
@@ -15,22 +15,20 @@ def one_hot_encoded_Targets(target_list, rows):
 
 
 def train(epoch, eta, data, labels, weights, targets):
-	correctList = []
-	
 	correct = 0
 	wrong = 0
-	rows, cols = data.shape
-	for i in range(0, rows):
+	num_rows = data.shape[0]
+	for r in range(num_rows):
 		# I need to check one row of data at a time to see if weights need to be updated
 		# subseuent rows of data will fire with the new weights
 
 		#                  [1x785] @ [785x10]
-		activations = np.dot(data[i], weights)
-		tk = labels[i]
+		activations = np.dot(data[r], weights)
+		tk = labels[r]
 		yk = np.argmax(activations)
 		output = np.where(activations > 0, 1, 0)
 		
-		# print("%d) Firing perceptron %d"% (i, yk) )     
+		# print("%d) Firing perceptron %d"% (i, yk) )
 
 		if tk == yk:
 			correct +=1
@@ -48,42 +46,35 @@ def train(epoch, eta, data, labels, weights, targets):
 				# data_k = data[i]  # [1x785]
 
 										# [1x785] @ [1x10]
-				delta_w = eta * np.outer(data[i],   delta)   # [785x 10]
+				delta_w = eta * np.outer(data[r],   delta)   # [785x 10]
 				weights = weights + delta_w
 				wrong += 1
 
 				# print("AFTER ", weights)
-	accuracy = correct / data.shape[0] 
+	accuracy = correct / num_rows 
 	# print('\n weight and new weight are same: ', np.array_equal(hold_weight, weights))
 	
-	
-	# if epoch > 0 and abs(correctList[epoch] - correctList[epoch - 1]) < 0.01:	
-	# 	return (correctList, weights, epochList, weightsList)
 	return weights, accuracy
-	# return accuracy
-
+	
 
 
 
 def test(data, weights, targets):
 	prediction_list = []
 	correct = 0
-	rows = data.shape[0]
+	num_rows = data.shape[0]
 
 	activations = np.dot(data, weights)
-	output = np.where(activations > 0, 1, 0)
 	
-	for i in range(0, rows):
-		tk = targets[i]
-		yk = np.argmax(activations[i])
+	for r in range(0, num_rows):
+		tk = targets[r]
+		yk = np.argmax(activations[r])
 		prediction_list.append(yk)
 		# print("%d) Firing perceptron %d"% (i, yk) )     
 		if tk == yk:
 			correct += 1
 			# print("Correct: %d"% correct)
-
-
-	accuracy = (correct / rows) 
+	accuracy = (correct / num_rows) 
 	
 	return accuracy, prediction_list
 
@@ -106,7 +97,6 @@ def accuracyGraph(correctList, test_accuracy_list):
 # confMat(test_data, test_labels, epochs, eta)
 def confMat(data, targets, epochs, learning, prediction_list):
 	# create conf matrix
-	target_cols = len(targets)
 	conf = np.zeros([10, 10], dtype=int)
 	correct = 0
 
@@ -129,12 +119,14 @@ def confMat(data, targets, epochs, learning, prediction_list):
 
 	np.savetxt("confusion matrix.csv", conf, delimiter=',')
 
+
+
 def read_data_normalize_and_add_bias(classes, features):
 	
 	data, labels = loadlocal_mnist(
 		images_path=features, 
 		labels_path=classes)
-	rows, cols = data.shape
+	rows = data.shape[0]
 
 	# normalize
 	data = data / 255
@@ -155,8 +147,10 @@ def main():
 	epochs = 70
 	eta = .1
 	batch = 1
-	
 
+	
+	
+	# files
 	train_data_file = 'data/train-images.idx3-ubyte'
 	train_label_file = 'data/train-labels.idx1-ubyte'	
 	test_data_file   = 'data/t10k-images.idx3-ubyte'
@@ -170,14 +164,16 @@ def main():
 	test_rows, test_cols = test_data.shape
 	train_rows, train_cols = train_data.shape
 
-	# we need 10 sets of weigts, one for each digit we want to id
-	weights = np.random.default_rng().uniform(-0.05, 0.05, [train_cols, 10])
 
-	# TODO
-	# FIXME for testing purposes only
-	np.random.seed(1)
-	weights = np.random.randint(-5, 5, [train_cols, 10])
-	weights = weights / 100
+	if __debug__:
+		# for testing purposes only
+		np.random.seed(1)
+		weights = np.random.randint(-5, 5, [train_cols, 10])
+		weights = weights / 100
+	else:
+		# we need 10 sets of weigts, one for each digit we want to id
+		weights = np.random.default_rng().uniform(-0.05, 0.05, [train_cols, 10])
+	
 
 	# when identifies digit 0 we get the zeroth row and one-hot matrix
 	targets = np.identity(10, dtype=int)
@@ -192,17 +188,18 @@ def main():
 		# train, update weights and  check training accuracy
 		weights, train_accuracy = train(epoch, eta, train_data, train_label, weights, targets)
 		train_accuracy_list.append(train_accuracy)
-		# test and check test accuracy
-		print('\nEpoch: %d\nTraining accuracy %0.2f' % (epoch, train_accuracy))
+		if __debug__:
+			print('\nEpoch: %d\nTraining accuracy %0.2f' % (epoch, train_accuracy))
 	
 		#                     data      weights    targets
 		test_accuracy, predictions = test(test_data, weights, test_labels)
 		test_accuracy_list.append(test_accuracy)
 		# prediction_list.append(prediction)
 
-		print('Test accuracy     %0.2f' % (test_accuracy))
-		lapse_time = time.time() - start
-		print("Epoch %d lasted %0.2f seconds"% (epoch, lapse_time))
+		if __debug__:
+			print('Test accuracy     %0.2f' % (test_accuracy))
+			lapse_time = time.time() - start
+			print("Epoch %d lasted %0.2f seconds"% (epoch, lapse_time))
 
 
 
